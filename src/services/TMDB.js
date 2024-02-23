ec2cbb0e3a3ae6a35507538bedc76b6a0d9602cc
Viplove Kale <80448124+viplove3541@users.noreply.github.com>
2024-02-23 14:42:53 +0530

    Update TMDB.js

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -11,7 +11,7 @@ export const tmdbApi = createApi({
       query: () => `genre/movie/list?api_key=${tmdbApiKey}`
     }),
 
-    //* Get Movies bye [type]
+    //* Get Movies by type
     getMovies: builder.query({
       query: ({ genreIdOrCategoryName, page, searchQuery }) => {
         
@@ -20,33 +20,39 @@ export const tmdbApi = createApi({
           return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
         }
               
-        //GET MOVIES BY CATEGORY
-        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
-          return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
+        //GET MOVIES BY CATEGORY or GENRE
+        let endpoint;
+        if (typeof genreIdOrCategoryName === 'string') {
+          endpoint = `movie/${genreIdOrCategoryName}`;
+        } else if (typeof genreIdOrCategoryName === 'number') {
+          endpoint = `discover/movie?with_genres=${genreIdOrCategoryName}`;
+        } else {
+          endpoint = 'movie/popular';
         }
-        //GET MOVIES BY GENRE
-        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
-          return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
-        }
-        //GET MOVIES BY POPULAR
-        return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+        
+        return `${endpoint}?page=${page}&api_key=${tmdbApiKey}`;
       },
     }),
+
     //GET MOVIE
     getMovie: builder.query({
       query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`
     }),
-    //Get user specific lisrt..
+
+    //Get user specific list
     getList: builder.query({
       query: ({ listName, accountId, sessionId, page }) => `/account/${accountId}/${listName}?api_key=${tmdbApiKey}&session_id=${sessionId}&page=${page}`,
     }),
+
     getRecommendations: builder.query({
       query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
     }),
-    //get Actors info
+
+    //get Actor info
     getActorsDetails: builder.query({
       query: (id) => `person/${id}?api_key=${tmdbApiKey}`,
     }),
+
     //GET MOVIES BY ACTOR
     getMoviesByActorId: builder.query({
       query: ({ id, page }) => `/discover/movie?with_cast=${id}&page=${page}&api_key=${tmdbApiKey}`,
